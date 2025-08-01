e17a4e85b6a77279ba1a19d70bc3571cbd6693a6
logansailer <logansailer>
2025-07-31 21:10:27 -0700

    completed selects for each page

--- project/app.js
+++ project/app.js
@@ -32,30 +32,6 @@ app.get('/', async function (req, res) {
     }
 });
 
-app.get('/bsg-people', async function (req, res) {
-    try {
-        // Create and execute our queries
-        // In query1, we use a JOIN clause to display the names of the homeworlds
-        const query1 = `SELECT bsg_people.id, bsg_people.fname, bsg_people.lname, \
-            bsg_planets.name AS 'homeworld', bsg_people.age FROM bsg_people \
-            LEFT JOIN bsg_planets ON bsg_people.homeworld = bsg_planets.id;`;
-        const query2 = 'SELECT * FROM bsg_planets;';
-        const [people] = await db.query(query1);
-        const [homeworlds] = await db.query(query2);
-
-
-        // Render the bsg-people.hbs file, and also send the renderer
-        //  an object that contains our bsg_people and bsg_homeworld information
-        res.render('bsg-people', { people: people, homeworlds: homeworlds });
-    } catch (error) {
-        console.error('Error executing queries:', error);
-        // Send a generic error message to the browser
-        res.status(500).send(
-            'An error occurred while executing the database queries.'
-        );
-    }
-});
-
 app.get('/passengers', async function (req, res) {
     try {
         const queryPassengers = `SELECT Passengers.idPassenger, Passengers.firstName, \
@@ -96,12 +72,19 @@ app.get('/routes', async function (req, res) {
 
 app.get('/schedules', async function (req, res) {
     try {
-        const querySchedules = 'SELECT * FROM Schedules'
+        const querySchedules = `SELECT Routes.name AS 'onRoute', Stations.name as 'atStation', \
+            Schedules.arrivalTime, Schedules.departureTime FROM Schedules \
+            LEFT JOIN Routes ON Schedules.idRoute = Routes.idRoute
+            LEFT JOIN Stations ON Schedules.idStation = Stations.idStation;`
+        const queryRoutes = 'SELECT * FROM Routes'
+        const queryStations = 'SELECT * FROM Stations'
         const [schedules] = await db.query(querySchedules);
+        const [routes] = await db.query(queryRoutes);
+        const [stations] = await db.query(queryStations);
 
         // Render the schedules.hbs file, and also send the renderer
         //  an object that contains the schedules entity from the database
-        res.render('schedules', { schedules: schedules });
+        res.render('schedules', { schedules: schedules,  routes: routes, stations: stations});
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
@@ -130,12 +113,16 @@ app.get('/stations', async function (req, res) {
 
 app.get('/trains', async function (req, res) {
     try {
-        const queryTrains = 'SELECT * FROM Trains'
+        const queryTrains = `SELECT Trains.idTrain, Trains.name, \
+            Trains.maxCapacity, Routes.name AS 'onRoute' FROM Trains \
+            LEFT JOIN Routes ON Trains.idRoute = Routes.idRoute;`
+        const queryRoutes = 'SELECT * FROM Routes'
         const [trains] = await db.query(queryTrains);
+        const [routes] = await db.query(queryRoutes);
 
         // Render the trains.hbs file, and also send the renderer
         //  an object that contains the trains entity from the database
-        res.render('trains', { trains: trains });
+        res.render('trains', { trains: trains, routes: routes });
     } catch (error) {
         console.error('Error executing queries:', error);
         // Send a generic error message to the browser
